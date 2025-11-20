5415c63f8776c5e875bedf9eeff4c2acf15730e4
Peter Barabas <peter.barabas@goto.com>
2025-11-20 19:33:27 +0100

    Add documentation

--- main.go
+++ main.go
@@ -1,3 +1,59 @@
+/*
+
+ - check numbers: -999..999
+
+
+===[ INSTRUCTION SET ]===
+
+5 bits for op code
+10 bits for numbers
+
+
+---[ PORTS ]---
+
+000 NIL
+001 Left
+010 Right
+011 Up
+100 Down
+101 Any
+110 Last
+
+
+---[ INSTRUCTIONS ]---
+
+00000000 NOP
+00001000 SWP
+00010000 SAV
+00011000 NEG
+
+00100000 MOV number to ACC
+00101000 MOV port to port
+00110000 MOV ACC to port
+00111000 MOV port to ACC
+
+01000000 ADD number
+01001000 ADD port
+
+01010000 SUB number
+01011000 SUB port
+
+01100000 JMP
+01101000 JEZ
+01110000 JNZ
+01111000 JGZ
+10000000 JLZ
+10001000 JRO offset
+10010000 JRO ACC
+
+
+---[ EXAMPLES ]---
+
+MOV 33, ACC
+00100000 00100001
+
+*/
+
 package main
 
 type Port struct {
