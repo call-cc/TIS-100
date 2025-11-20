75220e2dcca557216dafd3d67bd1b280fc3ba785
Peter Barabas <peter.barabas@goto.com>
2025-11-20 19:33:57 +0100

    Add code

--- main.go
+++ main.go
@@ -56,33 +56,108 @@ MOV 33, ACC
 
 package main
 
-type Port struct {
-	Name string
-	Enabled bool
-	Ready bool
-}
+import (
+	"fmt"
+)
 
 type Node struct {
-	Acc int
-	Bak int
+	Acc  int
+	Bak  int
 	Last string
-	Ports []Port
+	Code []uint8
+	PC   int
 }
 
-
-
-func Swp(n *Node) {
-	n.Acc, n.Bak = n.Bak, n.Acc
+type Port struct {
+	Node1     int
+	Node2     int
+	PortName1 string
+	PortName2 string
+	Value     int
 }
 
-func Sav(n *Node) {
-	n.Bak = n.Acc
+type Number int
+
+type Reg string
+
+type PortName string
+
+var Ports []Port
+
+var Nodes []Node
+
+func findPort(n int, port string) (int, error) {
+	for idx := range Ports {
+		if Ports[idx].Node1 == n && Ports[idx].PortName1 == port {
+			return idx, nil
+		}
+
+		if Ports[idx].Node2 == n && Ports[idx].PortName2 == port {
+			return idx, nil
+		}
+	}
+
+	return 0, fmt.Errorf("Port '%s' not found", port)
 }
 
-func Neg(n *Node) {
-	n.Acc = -n.Acc
+func Swp(n int) {
+	Nodes[n].Acc, Nodes[n].Bak = Nodes[n].Bak, Nodes[n].Acc
+}
+
+func Sav(n int) {
+	Nodes[n].Bak = Nodes[n].Acc
+}
+
+func AddNum(n, a int) {
+	Nodes[n].Acc += a
+}
+
+func SubNum(n, a int) {
+	Nodes[n].Acc -= a
+}
+
+func Nop(n int) {
+}
+
+func Neg(n int) {
+	Nodes[n].Acc = -Nodes[n].Acc
+}
+
+func MovNumReg(n int, src Number, dst Reg) {
+	// TODO types
+	Nodes[n].Acc = int(src)
+}
+
+func MovNumPort(n int, src Number, dst PortName) error {
+	// TODO types
+	idx, err := findPort(n, string(dst))
+	if err != nil {
+		return err
+	}
+
+	Ports[idx].Value = int(src)
+
+	return nil
+}
+
+func MovPortPort(n int, src PortName, dst PortName) {
+
+}
+
+func MovPortNil(n int, src PortName) {
+	// read from port and discard
+}
+
+func MovNilPort(n int, dst PortName) {
+	MovNumPort(n, 0, dst)
 }
 
 func main() {
+	Nodes = []Node{{}, {}, {}, {}}
+	Ports = []Port{
+		{0, 1, "Right", "Left", 0},
+		{0, 2, "Down", "Up", 0},
+		{1, 3, "Down", "Up", 0},
+		{2, 3, "Right", "Left", 0},
+	}
 }
-
