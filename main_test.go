44072bcc9f8dd4ccd351637dba02521e9b68dd5e
Peter Barabas <peter.barabas@goto.com>
2025-11-20 19:30:23 +0100

    Add tests

--- main_test.go
+++ main_test.go
@@ -5,48 +5,94 @@ import (
 )
 
 func TestSwp(t *testing.T) {
-	n := Node{
-		Acc:123,
-		Bak:456,
+	Nodes = []Node{
+		{
+			Acc: 123,
+			Bak: 456,
+		},
 	}
 
-	oldAcc := n.Acc
-	oldBak := n.Bak
+	oldAcc := Nodes[0].Acc
+	oldBak := Nodes[0].Bak
 
-	Swp(&n)
+	Swp(0)
 
-	if oldBak != n.Acc {
-		t.Errorf("old BAK %d != new ACC %d", oldBak, n.Acc)
+	if oldBak != Nodes[0].Acc {
+		t.Errorf("old BAK %d != new ACC %d", oldBak, Nodes[0].Acc)
 	}
 
-	if oldAcc != n.Bak {
-		t.Errorf("old ACC %d != new BAK %d", oldAcc, n.Bak)
+	if oldAcc != Nodes[0].Bak {
+		t.Errorf("old ACC %d != new BAK %d", oldAcc, Nodes[0].Bak)
 	}
 }
 
-func TestSav(t *testing.T) {
-	n := Node{
-		Acc:123,
+func TestAddNum(t *testing.T) {
+	tests := [][3]int{
+		{0, 0, 0},
+		{0, 1, 1},
+		{0, -1, -1},
+		{-5, -10, -15},
+		{12, 35, 47},
 	}
 
-	Sav(&n)
+	Nodes = []Node{{}}
 
-	if n.Bak != n.Acc {
-		t.Errorf("BAK %d != ACC %d", n.Bak, n.Acc)
+	for _, test := range tests {
+		Nodes[0].Acc = test[0]
+		AddNum(0, test[1])
+		if Nodes[0].Acc != test[2] {
+			t.Errorf("ACC %d + %d != %d", Nodes[0].Acc, test[1], test[2])
+		}
+	}
+}
+
+func TestSubNum(t *testing.T) {
+	tests := [][3]int{
+		{0, 0, 0},
+		{0, 1, -1},
+		{-1, -1, 0},
+		{-5, -3, -2},
+		{10, 1, 9},
+	}
+
+	Nodes = []Node{{}}
+
+	for _, test := range tests {
+		Nodes[0].Acc = test[0]
+		SubNum(0, test[1])
+		if Nodes[0].Acc != test[2] {
+			t.Errorf("ACC %d - %d != %d", Nodes[0].Acc, test[1], test[2])
+		}
+	}
+}
+
+func TestBak(t *testing.T) {
+	Nodes = []Node{
+		{
+			Acc: 123,
+		},
+	}
+
+	Sav(0)
+
+	if Nodes[0].Bak != Nodes[0].Acc {
+		t.Errorf("BAK %d != ACC %d", Nodes[0].Bak, Nodes[0].Acc)
 	}
 }
 
 func TestNeg(t *testing.T) {
 	vals := []int{-999, -100, 0, 50, 200}
 
-	n := Node{}
+	Nodes = []Node{
+		{},
+	}
 
-	for _, val := range vals{
-		n.Acc = val
-		Neg(&n)
+	for _, val := range vals {
+		Nodes[0].Acc = val
+		Neg(0)
 
-		if n.Acc != -val {
-			t.Errorf("ACC %d != -%d", n.Acc, -val)
+		if Nodes[0].Acc != -val {
+			t.Errorf("ACC %d != -%d", Nodes[0].Acc, -val)
 		}
 	}
 }
